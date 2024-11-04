84c6f4b53247960de19527c89f796013d330dfee
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-11-04 21:04:51 +0600

    update Skill section

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-document.getElementById('toggle-btn').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('collapsed');
-    const icon = document.querySelector('#toggle-btn i');
-    if (sidebar.classList.contains('collapsed')) {
-      icon.classList.remove('fa-chevron-left');
-      icon.classList.add('fa-chevron-right');
-    } else {
-      icon.classList.remove('fa-chevron-right');
-      icon.classList.add('fa-chevron-left');
-    }
-  });
+document.getElementById("toggle-btn").addEventListener("click", function () {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("collapsed");
+  const icon = document.querySelector("#toggle-btn i");
+  if (sidebar.classList.contains("collapsed")) {
+    icon.classList.remove("fa-chevron-left");
+    icon.classList.add("fa-chevron-right");
+  } else {
+    icon.classList.remove("fa-chevron-right");
+    icon.classList.add("fa-chevron-left");
+  }
+});
 
-  window.addEventListener('scroll', () => {
-  let sections = document.querySelectorAll('section');
-  let navLinks = document.querySelectorAll('.nav-menu a');
-  
-  sections.forEach(section => {
+window.addEventListener("scroll", () => {
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll(".nav-menu a");
+
+  sections.forEach((section) => {
     let top = window.scrollY;
     let offset = section.offsetTop - 100;
     let height = section.offsetHeight;
-    let id = section.getAttribute('id');
-    
+    let id = section.getAttribute("id");
+
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector('.nav-menu a[href*=' + id + ']').classList.add('active');
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(".nav-menu a[href*=" + id + "]")
+          .classList.add("active");
       });
     }
   });
